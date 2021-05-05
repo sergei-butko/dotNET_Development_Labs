7477bca5ed1ca4d53aa3f43283c74e44536699d4
sergei-butko <s.butko-2026@kpi.ua>
2021-05-05 17:41:22 +0300

    Project 3

---
+++ dotNET_Lab3/Program_Lab3/States/ConfirmedState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Program_Lab3.States
+{
+    public class ConfirmedState : IState
+    {
+        private readonly Grant _grant;
+
+        public ConfirmedState(Grant grant)
+        {
+            _grant = grant;
+        }
+
+        public void NextState() 
+        {            
+            Console.Clear();
+            _grant.ShowState();
+        }
+
+        public void ShowState()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nYour request was confirmed.");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void GetMoney()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\nCongratulations! You can take your money.");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+
+        public void GetRequestAmount()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n7 requests was confirmed.");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
---
+++ dotNET_Lab3/Program_Lab3/States/IState.cs
@@ -0,0 +1,10 @@
+﻿namespace Program_Lab3.States
+{
+    public interface IState
+    {
+        void ShowState();
+        void GetMoney();
+        void GetRequestAmount();
+        void NextState();
+    }
+}
